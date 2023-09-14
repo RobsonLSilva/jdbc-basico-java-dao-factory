47cc4b593d603455ddb4bad2b0b276d330c28cfb
Robson <robson.lsilva36@gmail.com>
2023-09-13 22:44:08 -0300

    Implementação do método selectById da classe SellerDaoJDBC, implementação de dois métodos auxiliares instantiateSeller e instantiateDepartment

--- src/robson/jdbc/exemplos/java/application/Main.java
+++ src/robson/jdbc/exemplos/java/application/Main.java
@@ -13,14 +13,18 @@ public class Main {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Department department = new Department(2,null);
+        Department department = new Department(1,null);
 
         System.out.println("===== TEST 1: seller insert =====");
         Seller newSeller;
-
-        newSeller = new Seller(null, "Greg", "greg@gmail.com", sdf.parse("26/06/1985"), 4000.0, department);
-
+        newSeller = new Seller(null, "Pedro", "pedro@gmail.com", sdf.parse("03/03/1990"), 2500.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("=======================================================");
+
+        System.out.println("=== TEST 2: seller findById =====");
+        Seller seller = sellerDao.findById(1);
+        System.out.println(seller);
     }
 }
--- src/robson/jdbc/exemplos/java/model/dao/implementations/SellerDaoJDBC.java
+++ src/robson/jdbc/exemplos/java/model/dao/implementations/SellerDaoJDBC.java
@@ -16,6 +16,24 @@ public class SellerDaoJDBC implements SellerDao {
         this.conn = conn;
     }
 
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException { // throws = propaga a exceção
+        Seller obj = new Seller();
+        obj.setId(rs.getInt("Id"));
+        obj.setName(rs.getString("Name"));
+        obj.setEmail(rs.getString("Email"));
+        obj.setBaseSalary(rs.getDouble("BaseSalary"));
+        obj.setBirthDate(rs.getDate("BirthDate"));
+        obj.setDepartment(dep);
+        return obj;
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString("DepName"));
+        return dep;
+    }
+
     @Override
     public void insert(Seller obj) {
         String sql = "INSERT INTO seller (name, email, birthDate, baseSalary, departmentId) VALUES (?, ?, ?, ?, ?)";
@@ -78,7 +96,7 @@ public class SellerDaoJDBC implements SellerDao {
         PreparedStatement st = null;
 
         try{
-            st = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            st = conn.prepareStatement(sql);
             st.setInt(1, id);
 
             st.executeUpdate();
@@ -91,6 +109,28 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
+        String sql = "SELECT seller.*, department.Name as DepName "
+                     +"FROM seller "
+                     +"INNER JOIN department ON seller.DepartmentId = department.Id "
+                     +"WHERE seller.Id = ?";
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(sql);
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if (rs.next()) {    // se veio alguma coisa na minha consulta
+                Department dep = instantiateDepartment(rs);
+                return instantiateSeller(rs, dep);
+            }
+            return null;
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
         return null;
     }
 
